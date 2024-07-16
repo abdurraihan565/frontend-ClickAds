e9cd4200f3d4bdea8c9f25f70671e430d01e9ec7
Abdur Rayhan <128666297+abdurraihan565@users.noreply.github.com>
2024-07-16 20:56:31 +0600

    Update Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 function Error() {
   return (
     <div className="error">
+    <Helmet>
+        <title>Contact Us</title>
+        <meta name="description" content="This is a description of my page" />
+        <meta
+          name="keywords"
+          content="make passive income,watch and earn money,passive money,passive revenue,static income,ideas of passive income,Earn Money Watching Ads,make passive income,Get Paid to Watch,earn passive income,amazon remote careers,survey monkey survey,get paid to watch,Paid to View,paid view point,paidview point,Ad Viewing Sites,Online Earnings Tips,Side Hustles,Earn Cash Online,Earn at Home,Earn Online Fast,Mobile Earnings,Social Media Marketing,Online Surveys,Forex Trading,bitcoin,Freelancing,work from home jobsremote data entry jobs,remote work part time,generate money online,affiliate marketingaffleting, marketingwatching advertisement earn money,Task Apps,Online Marketplaces,clickads,Career Development,watching ads and earn,top earn money site,online jobs,earn mone from home,make money online,passive income,advertise earning site,Cash for Ad Watching,Fast Cash,Get Paid Instantly,Ad Viewing Sites,Online Earnings,click to earn,typing jobs,writting jobs,work to earn,work from home,jobs online,online jobs,how to earn money online,how to get a jobs,how to earn money online,part time jobs for student,data entry jobs,online jobs,side hustles,earning apps,remote work,dropshing amazon,freelancing jobs,cpa marketing work,best earning site,make money online 2024,earn money per click,microjobs,microjobs site,microjobs work,jobs,work, ho wto get earn money,"
+        />
+      </Helmet>
       <h1>404 Not Found</h1>
     </div>
   );
