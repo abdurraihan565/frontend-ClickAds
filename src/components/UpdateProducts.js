0dcff2f85ef6dcfe4ffe2f1d879d35e29cd747db
Abdur Rayhan <128666297+abdurraihan565@users.noreply.github.com>
2024-07-16 11:53:39 +0600

    Update UpdateProducts.js

--- src/components/UpdateProducts.js
+++ src/components/UpdateProducts.js
@@ -10,7 +10,7 @@ function UpdateProducts() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const result = await axios.get('http://localhost:3002/api/products');
+        const result = await axios.get('https://clickads-backend.onrender.com/api/products');
         setProductData(result.data.payload.products);
       } catch (e) {
         if (e.response.data.message) {
